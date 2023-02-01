bcbe376f310e3af2f9139a663a10467296dacd96
Timothylai1121 <timothylai1121@gmail.com>
2023-02-01 07:34:18 +0200

    fixing js

--- assets/script/script.js
+++ assets/script/script.js
@@ -32,3 +32,53 @@ function updateClock() {
 // https://day.js.org/docs/en/display/format //
 var todayDate = dayjs('2019-01-25').format('DD/MM/YYYY');
 $("#currentDate").html(todayDate);
+
+$(document).ready(function () {
+  $('.saveBtn').click(function() {
+    var textareaValue = $(this).siblings('.textbox').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, textareaValue);
+  });
+});
+
+
+// adding color based on timing //
+function timeColorAM() {
+  var currentHour = dayjs().hour();
+  console.log(currentHour);
+
+$(".timeblock").each(function () {
+  var blockId = $(this).attr("id");
+  /* block id and leave hour alone */
+  /* parseInt is a method that parses a string argument and returns an integer. */
+  var blockTime = parseInt(blockId.split("am")[1]);
+  console.log(blockTime);
+
+  if (blockTime < currentHour) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+  } else if (blockTime === currentHour) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+  } else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+});
+}
+
+
+
+/* Testing color based on changing time in windows Date/Time */
+/* timeblock & icons showing color and not responding */
+
+/* currentTime is not defined in result showing only .future, html show future as class on all */
+/* using cdnjs to get latest version */
+/* after realizing that id in this case is senstive */
+/* changing it in am to see */
+/* console finally list current time and time in 24h */
+
+timeColorAM();
